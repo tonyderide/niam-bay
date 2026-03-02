6c12df5c29e59bea45dfa845002c64ab809065dc
Tony Deride <tderide.ext@galerieslafayette.com>
2026-03-02 05:32:57 +0100

    feat: add deployment config for Vercel (frontend) + Render (backend)

--- frontend/src/app/services/recipe.service.ts
+++ frontend/src/app/services/recipe.service.ts
@@ -3,14 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Recipe } from '../models/recipe.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeService {
+  private apiUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
   searchRecipes(ingredients: string[]): Observable<Recipe[]> {
     return this.http
-      .post<{ recipes: Recipe[] }>('/api/recipes/search', { ingredients })
+      .post<{ recipes: Recipe[] }>(`${this.apiUrl}/api/recipes/search`, { ingredients })
       .pipe(map(res => res.recipes));
   }
 }
